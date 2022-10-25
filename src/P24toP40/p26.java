e788835b31451a46d3dc1c155d9eec1a2f5ca3dd
coco-dogge <brian123483@yahoo.com.tw>
2022-10-25 22:42:01 +0800

    p26

---
+++ src/P24toP40/p26.java
@@ -0,0 +1,13 @@
+package P24toP40;
+
+public class p26 {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i <= 5){
+            System.out.println("第" + i + "次的迴圈");
+            i++;
+        }
+        System.out.println("end");
+    }
+}
