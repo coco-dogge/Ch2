47cfabe079b2f5d0e44ac7e60cb4263381494b61
coco-dogge <brian123483@yahoo.com.tw>
2022-10-25 22:23:17 +0800

    p15

---
+++ src/p15.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p15 {
+    public static void main(String[] args)throws IOException
+    {
+        System.out.println("in");
+
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        switch(num)
+        {
+            case 1:
+                System.out.println("in1");
+            case 2:
+                System.out.println("in2");
+            default:
+                System.out.println("input 1 or 2");
+        }
+
+    }
+}
