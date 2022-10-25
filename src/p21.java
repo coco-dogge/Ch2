d316f6601ed6d862db9dfca6f6845652a1b9fa9d
coco-dogge <brian123483@yahoo.com.tw>
2022-10-25 22:36:32 +0800

    p21

---
+++ src/p21.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p21 {
+    public static void main(String[] args)throws IOException
+    {
+        System.out.println("in");
+
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int res = Integer.parseInt(str);
+
+        char ans = (res == 1)?'A':'B';
+            System.out.println("choose" + ans + "line");
+    }
+}
